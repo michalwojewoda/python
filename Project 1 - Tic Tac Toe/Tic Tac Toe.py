99ea775dd5d45037aa143296cdeac8b9a3ae0270
Michał Wojewoda <mw.pryw@gmail.com>
2020-06-01 17:54:50 +0200

    add place marker, add win check

--- Project 1 - Tic Tac Toe/Tic Tac Toe.py
+++ Project 1 - Tic Tac Toe/Tic Tac Toe.py
@@ -31,4 +31,23 @@ def player_input():
     else: 
         return("O","X")
 
+#Write function that takes in the board list object, a marker, and desired position number 1-9 and assign it to the board.
 
+def place_marker(board, marker,position):
+
+    board[position] = marker 
+
+# write a function tat takes in a board and mark x o and then checks to see if that mark has won
+
+def win_check(board, mark):
+
+    # return boolean to check if true or false
+
+    return ((board[7] == board[8] == board[9] == mark) or # across
+    (board[4] == board[5] == board[6] == mark) or # across
+    (board[1] == board[2] == board[3] == mark) or #across 
+    (board[7] == board[4] == board[1] == mark) or #down
+    (board[8] == board[5] == board[2] == mark) or # down
+    (board[9] == board[6] == board[3] == mark) or # down
+    (board[7] == board[5] == board[3] == mark) or #diagonal
+    (board[9] == board[5] == board[1] == mark) #Diagonal
