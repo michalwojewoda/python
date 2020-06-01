60802ba91cb0538154997d9502d7f024808108c2
Michał Wojewoda <mw.pryw@gmail.com>
2020-06-01 17:43:56 +0200

    Add Assign players

--- Project 1 - Tic Tac Toe/Tic Tac Toe.py
+++ Project 1 - Tic Tac Toe/Tic Tac Toe.py
@@ -1,4 +1,6 @@
+#starting with creation of a board 
 def display_board(board):
+    print('\n'*100)
     print("    |     |   ")
     print(" "+board[1]+"  |  "+board[2]+"  |  "+board[3])
     print("----|-----|----")
@@ -9,3 +11,24 @@ def display_board(board):
 
 board = ["#","0","0","0","0","0","0","0","0","0"]
 print(display_board(board))
+print(display_board(board))
+
+#assign players
+
+def player_input():
+
+    '''
+    Output is tuple = (player 1 marker, Player 2 marker)
+
+    '''
+    marker = ""
+    while marker != 'X' and marker !='O':
+        marker = input("Player 1, choose X or O: ").upper()
+    
+    if marker == "x":
+
+        return ("X", "O")
+    else: 
+        return("O","X")
+
+
