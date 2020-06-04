640187a7534af4c2ebf7bb2d19b6b6bbc7c04fc8
Michał Wojewoda <mw.pryw@gmail.com>
2020-06-04 09:34:24 +0200

    finished logic

--- Project 1 - Tic Tac Toe/Tic Tac Toe.py
+++ Project 1 - Tic Tac Toe/Tic Tac Toe.py
@@ -1,5 +1,6 @@
-#starting with creation of a board 
 import random
+
+#starting with creation of a board 
 
 def display_board(board):
     print('\n'*100)
@@ -33,7 +34,8 @@ def player_input():
     else: 
         return("O","X")
 
-#Write function that takes in the board list object, a marker, and desired position number 1-9 and assign it to the board.
+# Write function that takes in the board list object, a marker, 
+# and desired position number 1-9 and assign it to the board.
 
 def place_marker(board, marker,position):
 
@@ -56,8 +58,6 @@ def win_check(board, mark):
 
 # Random module - to randomly choose player that goes first
 
-import random
-
 def choose_first():
 
     flip = random.randint(0,1)
@@ -72,4 +72,108 @@ def space_check(board, position):
 
     return board[position] == " "
 
-#check if the board is fulli
+#check if the board is full #Day2
+
+def full_board_check(board):
+    for i in range(1,10):
+        if space_check(board.i):
+            return False
+    return True # that says boar is full
+
+#players next position 
+
+def playerchoice(board):
+    position = 0
+    while position not in [1,2,3,4,5,6,7,8,9,] or full_board_check(board,position):
+        position = int(input('choose a position (1-9 '))
+    return position
+
+#ask player if he want to play again
+
+def replay():
+    return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+
+# Logic 
+
+#first while looop to keep running the game
+
+print("Do you want to play Tic Tac Toe")
+
+while True:
+    #play the game
+
+    ## set evertytnig up
+
+    the_board = [' ']*10
+    player1_marker,player2_marker = player_input()
+
+    turn = choose_first()
+    print(turn+' will go first'):
+
+    play_game = input("Ready to play? y or n")
+
+    if play_game == 'y':
+        game_on = True
+    else:
+        game_on = False 
+
+    ##Game play
+
+    while game_on:
+
+        if turn == 'Player 1':
+            # show the board
+            display_board(the_board)
+            #choose a position
+            position = playerchoice(the_board)
+
+            #place the marker on the position
+            place_marker(the_board,player1_marker,position)
+
+            #check if they won
+            if win_check(the_board,player1_marker):
+                display_board(the_board)
+                print('player 1 has won!')
+                game_on = False
+            # or check if there is a tie
+            else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print("Tie Game!")
+                    break
+            #no tie and no win? then next players turn
+                else:
+                    turn = "Player 2"
+
+        
+
+    ##Player 1
+
+        else:
+            
+            display_board(the_board)
+            #choose a position
+            position = playerchoice(the_board)
+
+            #place the marker on the position
+            place_marker(the_board,player2_marker,position)
+
+            #check if they won
+            if win_check(the_board,player2_marker):
+                display_board(the_board)
+                print('player 2 has won!')
+                game_on = False
+            # or check if there is a tie
+            else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print("Tie Game!")
+                    break
+            #no tie and no win? then next players turn
+                else:
+                    turn = "Player 1"
+
+    if not replay():
+        break
+
+#Break out of the while loop on replay
