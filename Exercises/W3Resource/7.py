a2c55e8e4e1d46033ea097f8f96819b6a495ddf4
Michał Wojewoda <mw.pryw@gmail.com>
2020-10-09 10:30:29 +0200

    Check Check

--- Exercises/W3Resource/7.py
+++ Exercises/W3Resource/7.py
@@ -2,3 +2,18 @@
 filename = input("Please input here the Filename: \n")
 f_extns = filename.split(".")
 print ("The extension of the file is : " + repr(f_extns[-1]))
+
+# Write a Python program to accept a filename from the user and print the extension of that.
+filename = input("Please input here the Filename: \n")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+
+# Write a Python program to accept a filename from the user and print the extension of that.
+filename = input("Please input here the Filename: \n")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+
+# Write a Python program to accept a filename from the user and print the extension of that.
+filename = input("Please input here the Filename: \n")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
